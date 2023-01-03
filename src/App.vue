880c426444e8f5302a9417af50f971047f7f4eb8
coldTOUFU <coldtoufu.dev@gmail.com>
2023-01-03 22:42:10 +0900

    add: レイアウトとかは無視で、とにかく必要な情報を表示できるようにした。

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-
 const UnoColor = {
   Red:    "red",
   Yellow: "yellow",
@@ -19,19 +18,19 @@ const UnoPattern = {
   Seven:            "7",
   Eight:            "8",
   Nine:             "9",
-  Skip:             "skip",
-  Reverse:          "reverse",
-  DrawTwo:          "draw_two",
-  Wild:             "wild",
-  WildDraw4:        "wild_draw_4",
-  WildShuffleHands: "wild_shuffle_hands",
-  WildCustomizable: "wild_customizable"
+  Skip:             "Skip",
+  Reverse:          "Rev",
+  DrawTwo:          "D2",
+  Wild:             "W",
+  WildDraw4:        "WD4",
+  WildShuffleHands: "WSH",
+  WildCustomizable: "WC"
 };
 
 export default {
   data() {
     return {
-      state_idx: 0,
+      stateIdx: 0,
       states:    []
     }
   },
@@ -94,6 +93,18 @@ export default {
         this.states = JSON.parse(event.target.result);
       }
       reader.readAsText(file)
+    },
+    incrementStateIdx() {
+      this.stateIdx++;
+      if (this.stateIdx >= this.states.length) {
+        this.stateIdx = this.states.length;
+      }
+    },
+    decrimentStateIdx() {
+      this.stateIdx--;
+      if (this.stateIdx <= 0) {
+        this.stateIdx = 0;
+      }
     }
   }
 
@@ -101,25 +112,82 @@ export default {
 </script>
 
 <template>
-  <input type="file" ref="file" @change="loadLog"/>
-  <br>
-  <table v-if="states.length > 0">
-    <thead>
-      <tr><th>カード</th></tr>
-    </thead>
-    <tbody>
-      <tr v-for="playerCards in states[state_idx].playerCards">
-        <td>
-          <div v-for="card in playerCards" :class="parseColor(card)">
-          {{ card }}
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <h2>ファイル読み込み</h2>
+    <p>UnoStateのJSONを読み込ませてください。</p>
+    <input type="file" ref="file" @change="loadLog"/>
+  </div>
+  <div v-if="states.length > 0">
+    <h2>表示中の状態</h2>
+    <p>状態: {{ stateIdx + 1 }} / {{ states.length }}</p>
+    <input type="button" value="前状態" @click="decrimentStateIdx">
+    <input type="button" value="次状態" @click="incrementStateIdx">
+  </div>
+  <div v-if="states.length > 0">
+    <h2>テーブル情報</h2>
+    <div>
+      <ul>
+        <li>現在のプレイヤ: {{ states[stateIdx].currentPlayer }}</li>
+        <li>周順: {{ states[stateIdx].isNormalOrder ? "正順" : "逆順" }}</li>
+        <li v-if="states[stateIdx].boundPlayer !== -1">Bindされているプレイヤ: {{ states[stateIdx].boundPlayer }}</li>
+        <li v-if="states[stateIdx].boundPlayer !== -1">Bind2の残りターン: {{ states[stateIdx].boundTurn }}</li>
+      </ul>
+      <h3>場のカード</h3>
+      <div :class="['card', parseColor(states[stateIdx].tableColor), {'wild-customizable' : (parsePattern(states[stateIdx].tablePattern) === 'W')}]">
+        {{ parsePattern(states[stateIdx].tablePattern) }}
+      </div>
+    </div>
+    <h2>プレイヤ情報</h2>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>プレイヤの席番号</th>
+            <th>プレイヤの得点</th>
+            <th>プレイヤの手札</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="playerIdx in [0, 1, 2, 3]">
+            <td>
+              {{ states[stateIdx].playerSeats[playerIdx] }}
+            </td>
+            <td>
+              {{ states[stateIdx].playerScores[playerIdx] }}
+            </td>
+            <td>
+              <div class="cards">
+                <div v-for="card in states[stateIdx].playerCards[playerIdx]" :class="['card', parseColor(card), {'wild-customizable' : (parsePattern(card) === 'W')}]">
+                  {{ parsePattern(card) }}
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <style>
+  .cards {
+    display: flex;
+  }
+
+  .card {
+    width: 3.5em;
+    height: 5em;
+    border-radius: 0.25em;
+    border: 0.1em solid black;
+    padding: 0.1em;
+    margin: 0.1em 0.1em;
+    font-size: 1em;
+    font-weight: bold;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  /* カードの色。 */
   .red {
     color: white;
     background-color: red;
@@ -139,5 +207,16 @@ export default {
   .wild {
     color: white;
     background-color: black; 
+  }
+
+  /* カードの模様。色に優先。 */
+  .wild-customizable {
+    background: linear-gradient(to bottom,
+        black 40%, red 40% 55%, yellow 55% 70%, green 70% 85%, blue 85%) !important;
+  }
+
+  /* divの前後を改行。 */
+  div {
+    white-space: normal;
   }
 </style>
